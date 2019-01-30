a4954a7c878c93cacf1397fb357d462db5586644
Paulo Demutti <paulodemutti@gmail.com>
2019-01-30 21:36:26 -0200

    homeComponent added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from '../app/components/feature/home/home.component';
+
+import { HomeResolverService } from '../app/services/home-resolver.service';
+import { AuthenticationGuard } from './guards/authentication.guard';
+
+const routes: Routes = [
+  { 
+    path: 'home', 
+    component: HomeComponent, 
+    canActivate: [ AuthenticationGuard ], 
+    resolve: { 
+      repos: HomeResolverService 
+    } 
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
