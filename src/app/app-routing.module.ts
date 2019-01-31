950b508b274739435cdf880f08dd6581132a22b3
Paulo Demutti <paulodemutti@gmail.com>
2019-01-30 22:38:59 -0200

    login added to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from '../app/components/feature/home/home.component';
+import { LoginComponent } from '../app/components/feature/login/login.component';
 
 import { HomeResolverService } from '../app/services/home-resolver.service';
 import { AuthenticationGuard } from './guards/authentication.guard';
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
   { 
     path: 'home', 
     component: HomeComponent, 
