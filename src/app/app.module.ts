414694967e07871d93e31f02359a357751cea310
Paulo Demutti <paulodemutti@gmail.com>
2019-01-30 22:20:23 -0200

    App, Header, Home and Repository component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/common/app/app.component';
+import { HomeComponent } from '../app/components/feature/home/home.component';
+
+import { HeaderComponent } from './components/common/header/header.component';
+import { RepositoryComponent } from './components/common/repository/repository.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    RepositoryComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
