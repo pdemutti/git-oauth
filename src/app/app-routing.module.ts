f6cbd7cb40d0b1d4f947f0205c288296f7e9e920
Paulo Demutti <paulodemutti@gmail.com>
2019-01-30 23:58:56 -0200

    adding callback to routes, default routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from '../app/components/feature/home/home.component';
 import { LoginComponent } from '../app/components/feature/login/login.component';
+import { CallbackComponent } from '../app/components/feature/callback/callback.component';
 
 import { HomeResolverService } from '../app/services/home-resolver.service';
 import { AuthenticationGuard } from './guards/authentication.guard';
 
 const routes: Routes = [
+  { path: '', pathMatch: 'full', component: LoginComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'callback', component: CallbackComponent },
   { 
     path: 'home', 
     component: HomeComponent, 
@@ -17,6 +20,7 @@ const routes: Routes = [
       repos: HomeResolverService 
     } 
   },
+  { path: '**', component: LoginComponent }
 ];
 
 @NgModule({
